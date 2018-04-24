04c0c8d3b407e0a1cfa8de0c5e4a10be69716f97
Jia Jun Yang <jyang@baufest.com>
2018-04-24 10:07:47 -0300

    otra forma de encadenar promises

--- 01-hola-mundo/promesas.js
+++ 01-hola-mundo/promesas.js
@@ -55,3 +55,11 @@ getEmpleado(1).then(empleado =>{
 }, (err) => {
   console.log(err);
 });
+
+getEmpleado(10).then( empleado => {
+  return getSalario(empleado);
+  }).then(resp => {
+    console.log(`El salario de ${resp.nombre} es de ${ resp.salario}`);
+  }).catch( err => {
+    console.log(err);
+  })
