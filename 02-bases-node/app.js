9f9e049bfbb47511ef3e4aef45519358288ab554
Jia Jun Yang <jyang@baufest.com>
2018-04-24 12:38:47 -0300

    Agregado de YARGS en package y verificar argumentos de entrada por consola/terminal

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -1,17 +1,38 @@
+const argv = require('yargs')
+                    .command('listar','Imprime en consola la tabla de multiplicar',{
+                      base: {
+                        demand: true,
+                        alias: 'b'
+                      },
+                      limite: {
+                        alias: 'l',
+                        default: 10
+                      }
+                    })
+                    .help()
+                    .argv;
+
+
 // "importar del modulo multiplicar"
 // const multiplicar = require('./multiplicar/multiplicar');
 const { crearArchivo } = require('./multiplicar/multiplicar');
 // let base = '5';
 // console.log(module);
-console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
-// console.log(multiplicar);
-console.log(parametro);
-console.log(base);
+// console.log(process.argv);
+let argv2 = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+// // console.log(multiplicar);
+// console.log(parametro);
+// console.log(base);
+console.log(argv2);
+console.log(argv);
+console.log(argv.base);
+console.log(argv.limite);
 
 
-crearArchivo(base)
-  .then( archivo => console.log(`Archivo creado ${archivo}`))
-  .catch( err => console.log(err))
+
+
+// crearArchivo(base)
+//   .then( archivo => console.log(`Archivo creado ${archivo}`))
+//   .catch( err => console.log(err))
