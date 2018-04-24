c6709fac7c923c731a46348da1a94db0cdb72401
Jia Jun Yang <jyang@baufest.com>
2018-04-24 11:44:44 -0300

    pasa numero como strings

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -1,7 +1,7 @@
 // "importar del modulo multiplicar"
 // const multiplicar = require('./multiplicar/multiplicar');
 const { crearArchivo } = require('./multiplicar/multiplicar');
-let base = 's';
+let base = '5';
 // console.log(module);
 // console.log(multiplicar);
 crearArchivo(base)
