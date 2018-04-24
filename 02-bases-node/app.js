4417c79d51702585a425480dd1d6dc1b19e14084
Jia Jun Yang <jyang@baufest.com>
2018-04-24 12:08:28 -0300

    primer paso por parametro base

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -1,9 +1,17 @@
 // "importar del modulo multiplicar"
 // const multiplicar = require('./multiplicar/multiplicar');
 const { crearArchivo } = require('./multiplicar/multiplicar');
-let base = '5';
+// let base = '5';
 // console.log(module);
+console.log(process.argv);
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
 // console.log(multiplicar);
+console.log(parametro);
+console.log(base);
+
+
 crearArchivo(base)
   .then( archivo => console.log(`Archivo creado ${archivo}`))
   .catch( err => console.log(err))
