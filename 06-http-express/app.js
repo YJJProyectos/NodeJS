f52a49c8dafab2bded9bce5e2bf33fec34b686c0
Jia Jun Yang <jyang@baufest.com>
2018-04-25 11:11:53 -0300

    uso de HBS ruteando home y footer

--- 06-http-express/app.js
+++ 06-http-express/app.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
+app.use( express.static( __dirname + '/public') );
+
+// express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
  
+// app.get('/',  (req, res) => {
+//   // res.send('Hello');
+//   let salida = {
+//     nombre: 'fernando',
+//     edad: 23,
+//     url: req.url
+//   };
+//   res.send(salida);
+// });
+
 app.get('/',  (req, res) => {
-  // res.send('Hello');
-  let salida = {
-    nombre: 'fernando',
-    edad: 23,
-    url: req.url
-  };
-  res.send(salida);
+
+  res.render('home', {
+    nombre: 'pepe',
+    user: 'test',
+    anio: new Date().getFullYear()
+  });
+
 });
-app.get('/saludo',  (req, res) => {
-  res.send('Hello');
+
+
+app.get('/about',  (req, res) => {
+  res.render('about', {
+    anio: new Date().getFullYear()
+  });
 }); 
+
 app.listen(3000, () => {
   console.log('escuchando');
 });
